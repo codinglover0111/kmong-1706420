76fbc8aae0c8b2b69bf00d74c85c179295afb0aa
jungmin jo <cjm01115@naver.com>
2025-01-22 02:45:00 +0900

    FIX bug log created every single minute

--- src/main/ipcFunctions.ts
+++ src/main/ipcFunctions.ts
@@ -2,17 +2,20 @@ import { ipcMain } from 'electron'
 import * as fs from 'fs'
 
 const fsAsync = fs.promises
+// if i use this date in the function then it will make log file
+// every 1 minute so i put it here
+const date = new Date()
 
 export function ipcsINIT(): void {
+  // Ping! Pong!
   ipcMain.on('ping', () => console.log('pong'))
 
-  // log funtions
+  // log save to file and print to console with level
   ipcMain.on('log', async (_event, level: string = 'info', msg: string) => {
     console.log(`[${level.toUpperCase()}] ${msg}`)
 
     fs.existsSync('./logs') || fs.mkdirSync('./logs')
 
-    const date = new Date()
     await fsAsync.appendFile(
       `./logs/${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}.log`,
       `[${level.toUpperCase()}] ${msg}\n`
